561850f88c8f74305c275af8a4705501888ef371
telepancake <113372415+telepancake@users.noreply.github.com>
2026-04-11 12:16:28 +0300

    Delete .proctrace.ko.cmd

--- .proctrace.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_proctrace.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /usr/src/linux-headers-6.17.0-1010-azure/scripts/module.lds -o proctrace.ko proctrace.o proctrace.mod.o .module-common.o
